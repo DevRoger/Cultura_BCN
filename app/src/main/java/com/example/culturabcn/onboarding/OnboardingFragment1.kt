2a3d32cd25693f7820069d43af9ed56e4871b514
DevRoger <ralonsop2324@politecnics.barcelona>
2025-03-17 17:11:23 +0100

    Implementados botones de 'Skip' en OnBoarding.

--- app/src/main/java/com/example/culturabcn/onboarding/OnboardingFragment1.kt
+++ app/src/main/java/com/example/culturabcn/onboarding/OnboardingFragment1.kt
@@ -1,8 +1,10 @@
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
+import com.example.culturabcn.MainActivity
 import com.example.culturabcn.R
 
 class OnboardingFragment1 : Fragment(R.layout.fragment_onboarding_1) {
@@ -11,10 +13,19 @@ class OnboardingFragment1 : Fragment(R.layout.fragment_onboarding_1) {
         super.onViewCreated(view, savedInstanceState)
 
         val btnContinuarF1 = view.findViewById<Button>(R.id.btnContinuarF1)
+        val btnSaltarF1 = view.findViewById<Button>(R.id.btnSaltar1)
 
         btnContinuarF1.setOnClickListener {
             val viewPager2 = requireActivity().findViewById<ViewPager2>(R.id.viewPager)  // Obtener el ViewPager2 de la actividad
             viewPager2.currentItem = 1
+        }
+
+        btnSaltarF1.setOnClickListener {
+            // Redirigir al usuario a la MainActivity
+            val intent = Intent(requireActivity(), MainActivity::class.java)
+            startActivity(intent)
+
+            requireActivity().finish()  // Termina la actividad de onboarding para que no pueda volver
         }
     }
 }
--- app/src/main/java/com/example/culturabcn/onboarding/OnboardingFragment2.kt
+++ app/src/main/java/com/example/culturabcn/onboarding/OnboardingFragment2.kt
@@ -1,8 +1,10 @@
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
+import com.example.culturabcn.MainActivity
 import com.example.culturabcn.R
 
 class OnboardingFragment2 : Fragment(R.layout.fragment_onboarding_2) {
@@ -11,9 +13,18 @@ class OnboardingFragment2 : Fragment(R.layout.fragment_onboarding_2) {
         super.onViewCreated(view, savedInstanceState)
 
         val btnContinuarF2 = view.findViewById<Button>(R.id.btnContinuarF2)
+        val btnSaltarF2 = view.findViewById<Button>(R.id.btnSaltar2)
 
         btnContinuarF2.setOnClickListener {
             val viewPager2 = requireActivity().findViewById<ViewPager2>(R.id.viewPager)  // Obtener el ViewPager2 de la actividad
             viewPager2.currentItem = 3
+        }
+
+        btnSaltarF2.setOnClickListener {
+            // Redirigir al usuario a la MainActivity
+            val intent = Intent(requireActivity(), MainActivity::class.java)
+            startActivity(intent)
+
+            requireActivity().finish()  // Termina la actividad de onboarding para que no pueda volver
         }
     }}
