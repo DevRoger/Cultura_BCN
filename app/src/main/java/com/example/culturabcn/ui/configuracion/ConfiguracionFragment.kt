bdf22de28fbaa4e8f84e4f89b1af25edf0e0fcf7
DevRoger <ralonsop2324@politecnics.barcelona>
2025-05-14 19:56:58 +0200

    Implementado el cambio de idioma.

--- app/src/main/java/com/example/culturabcn/ui/configuracion/ConfiguracionFragment.kt
+++ app/src/main/java/com/example/culturabcn/ui/configuracion/ConfiguracionFragment.kt
@@ -1,11 +1,15 @@
 package com.example.culturabcn.ui.configuracion
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import com.example.culturabcn.R
 import com.example.culturabcn.databinding.FragmentConfiguracionBinding
+import java.util.Locale
 
 class ConfiguracionFragment : Fragment() {
 
@@ -16,6 +20,26 @@ class ConfiguracionFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
                              ): View {
         _binding = FragmentConfiguracionBinding.inflate(inflater, container, false)
+
+
+        val imgEspana = binding.imgEspana
+        val imgCatalan = binding.imgCatalan
+        val imgIngles = binding.imgIngles
+
+
+        imgEspana.setOnClickListener {
+            setLocale("es") // Español
+        }
+
+        imgCatalan.setOnClickListener {
+            setLocale("ca") // Catalán
+        }
+
+        imgIngles.setOnClickListener {
+            setLocale("en") // Inglés
+        }
+
+
         return binding.root
     }
 
@@ -23,4 +47,21 @@ class ConfiguracionFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    private fun setLocale(languageCode: String) {
+        val locale = Locale(languageCode)
+        Locale.setDefault(locale)
+
+        val config = Configuration()
+        config.setLocale(locale)
+
+        requireActivity().baseContext.resources.updateConfiguration(
+            config,
+            requireActivity().baseContext.resources.displayMetrics
+                                                                   )
+
+        // Reinicia la actividad actual para aplicar el nuevo idioma
+        requireActivity().recreate()
+    }
+
 }
